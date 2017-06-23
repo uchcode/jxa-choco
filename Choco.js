d20d2902fe65791f229653a4f87b152740e69ee8
Tom.U <tom@tom-macmini.local>
2017-06-23 10:12:01 +0900

    Fix: check 'globalize'

--- Choco.js
+++ Choco.js
@@ -359,6 +359,7 @@
 	global.Choco.doShell = doShell
 	
 	function globalize(scope) {
+		if (scope['globalize']) throw `globalize is exist, abort.`
 		Object.keys(global.Choco).forEach( i => {
 			if (i === 'globalize') return
 			if (scope[i]) throw `${i} is exist, abort.`
